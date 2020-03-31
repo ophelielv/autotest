6de64a9955cdd9d60c7a3467faee81192f307fef
Ophélie Le Vigouroux <levigouroux.oph@gmail.com>
2020-03-31 18:29:02 +0200

    mocha reporter in variable

--- autotestBack/routes/index.js
+++ autotestBack/routes/index.js
@@ -1,54 +1,72 @@
-var express = require('../node_modules/express');
-var router = express.Router();
+const express = require('../node_modules/express');
+const router = express.Router();
+const Mocha = require('mocha');
+
+const TESTS_DIR = 'suites';
+
+/**
+ * GET home page.
+ */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: `Express home` });
+});
+
+/**
+ * GET users page.
+ */
+router.get('/users', function (req, res, next) {
+  const mocha = new Mocha({
+    ui: 'bdd',
+    reporter: 'list'
+  });
+
+  mocha.addFile(`${TESTS_DIR}/test2.spec.js`);
+
+  mocha.run(/*function (failures) {
+    process.exitCode = failures ? 1 : 0;  // exit with non-zero status if there were failures
+  }*/)
+    .on('test', function (test) {
+      console.log('Test started: ' + test.title);
+    })
+    .on('test end', function (test) {
+      console.log('Test done: ' + test.title);
+    })
+    .on('pass', function (test) {
+      console.log('Test passed');
+      // console.log(test);
+    })
+    .on('fail', function (test, err) {
+      console.log('Test fail');
+      // console.log(test);
+      // console.log(err);
+    })
+    .on('end', function () {
+      console.log('All done');
+    });
+
+
+  res.render('index', { title: 'Users' });
+});
+
+module.exports = router;
+
 // const { Builder, By, Key, until } = require('selenium-webdriver');
 // const { expect } = require('chai');
 
-const Mocha = require('mocha'),
-  fs = require('fs'),
-  path = require('path');
-
-// Instantiate a Mocha instance.
-const mocha = new Mocha();
-const testDir = 'suites'
+// ,
+//   fs = require('fs'),
+//   path = require('path');
 
 /* EXEMPLE WITH MULTIPLE TESTS FILES
 // Add each .js file to the mocha instance
 // Only keep the .js files
-fs.readdirSync(testDir).filter(file => file.substr(-3) === '.js')
+fs.readdirSync(TESTS_DIR).filter(file => file.substr(-3) === '.js')
 .forEach(file => {
   mocha.addFile(
-    path.join(testDir, file)
+    path.join(TESTS_DIR, file)
   );
 });
 
 mocha.run(function (failures) {
   process.exitCode = failures ? 1 : 0;  // exit with non-zero status if there were failures
 });*/
-
-// GET home page.
-router.get('/', function (req, res, next) {
-  const obj = {
-    a: 1,
-    b: 54
-  };
-
-  const { a, b } = obj;
-
-  res.render('index', { title: `Express ${a} ${b}` });
-});
-
-
-
-// GET users page.
-router.get('/users', function (req, res, next) {
-  // Add one file
-  mocha.addFile(`${testDir}/test2.spec.js`);
-
-  mocha.run(function (failures) {
-    process.exitCode = failures ? 1 : 0;  // exit with non-zero status if there were failures
-  });
-
-  res.render('index', { title: 'Users' });
-});
-
-module.exports = router;
