d9380ef8664cc236e01c1b2d611cb94e271fd09f
Ophélie Le Vigouroux <levigouroux.oph@gmail.com>
2020-04-04 11:24:24 +0200

    mocha in service

--- autotestBack/routes/index.js
+++ autotestBack/routes/index.js
@@ -1,8 +1,6 @@
 const express = require('../node_modules/express');
 const router = express.Router();
-const Mocha = require('mocha');
-
-const TESTS_DIR = 'suites';
+const MochaService = require('../services/mocha.service');
 
 /**
  * GET home page.
@@ -15,58 +13,10 @@ router.get('/', function (req, res, next) {
  * GET users page.
  */
 router.get('/users', function (req, res, next) {
-  const mocha = new Mocha({
-    ui: 'bdd',
-    reporter: 'list'
-  });
-
-  mocha.addFile(`${TESTS_DIR}/test2.spec.js`);
-
-  mocha.run(/*function (failures) {
-    process.exitCode = failures ? 1 : 0;  // exit with non-zero status if there were failures
-  }*/)
-    .on('test', function (test) {
-      console.log('Test started: ' + test.title);
-    })
-    .on('test end', function (test) {
-      console.log('Test done: ' + test.title);
-    })
-    .on('pass', function (test) {
-      console.log('Test passed');
-      // console.log(test);
-    })
-    .on('fail', function (test, err) {
-      console.log('Test fail');
-      // console.log(test);
-      // console.log(err);
-    })
-    .on('end', function () {
-      console.log('All done');
-    });
-
+  const mochaService = new MochaService('test2');
+  mochaService.run();
 
   res.render('index', { title: 'Users' });
 });
 
 module.exports = router;
-
-// const { Builder, By, Key, until } = require('selenium-webdriver');
-// const { expect } = require('chai');
-
-// ,
-//   fs = require('fs'),
-//   path = require('path');
-
-/* EXEMPLE WITH MULTIPLE TESTS FILES
-// Add each .js file to the mocha instance
-// Only keep the .js files
-fs.readdirSync(TESTS_DIR).filter(file => file.substr(-3) === '.js')
-.forEach(file => {
-  mocha.addFile(
-    path.join(TESTS_DIR, file)
-  );
-});
-
-mocha.run(function (failures) {
-  process.exitCode = failures ? 1 : 0;  // exit with non-zero status if there were failures
-});*/
---
+++ autotestBack/services/mocha.service.js
@@ -0,0 +1,61 @@
+// const { expect } = require('chai');
+const Mocha = require('mocha');
+// ,
+//   fs = require('fs'),
+//   path = require('path');
+
+class MochaService {
+
+  constructor(testName) {
+
+    const TESTS_DIR = 'suites';
+    this.mocha = new Mocha(/*{
+      ui: 'bdd',
+      reporter: 'list'
+    }*/);
+
+    this.mocha.addFile(`${TESTS_DIR}/${testName}.spec.js`);
+  }
+
+  run() {
+    this.mocha.run(/*function (failures) {
+      process.exitCode = failures ? 1 : 0;  // exit with non-zero status if there were failures
+    }*/)
+      .on('test', function (test) {
+        console.log('Test started: ' + test.title);
+      })
+      .on('test end', function (test) {
+        console.log('Test done: ' + test.title);
+      })
+      .on('pass', function (test) {
+        console.log('Test passed');
+        // console.log(test);
+      })
+      .on('fail', function (test, err) {
+        console.log('Test fail');
+        // console.log(test);
+        // console.log(err);
+      })
+      .on('end', function () {
+        console.log('All done');
+      })
+      ;
+  }
+
+}
+
+module.exports = MochaService;
+
+/* EXEMPLE WITH MULTIPLE TESTS FILES
+// Add each .js file to the mocha instance
+// Only keep the .js files
+fs.readdirSync(TESTS_DIR).filter(file => file.substr(-3) === '.js')
+.forEach(file => {
+  mocha.addFile(
+    path.join(TESTS_DIR, file)
+  );
+});
+
+mocha.run(function (failures) {
+  process.exitCode = failures ? 1 : 0;  // exit with non-zero status if there were failures
+});*/
