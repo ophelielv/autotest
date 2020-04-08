f1ec227bb6fa6a00b1be9873c92a869b8c62797f
Ophélie Le Vigouroux <levigouroux.oph@gmail.com>
2020-04-09 01:12:20 +0200

    prepare API

--- autotestBack/routes/index.js
+++ autotestBack/routes/index.js
@@ -2,6 +2,10 @@ const express = require('../node_modules/express');
 const router = express.Router();
 const MochaService = require('../services/mocha.service');
 
+const bodyParser = require('body-parser');
+router.use(bodyParser.json()); // support json encoded bodies
+router.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
 /**
  * GET home page.
  */
@@ -18,6 +22,43 @@ router.get('/users', async (req, res, next) => {
   console.log('RESULT: ', results);
 
   res.render('index', { title: 'Users ' + JSON.stringify(results) });
+});
+
+/**
+ * Get the list of all tests
+ */
+router.get("/get-all/", (req, res, next) => {
+  res.json({ message: `get test id: ${req.params.id}` });
+});
+
+/**
+ * Get info about a test suite + previous results
+ */
+router.get("/get/:id", (req, res, next) => {
+  res.json({ message: `Get test id: ${req.params.id}` });
+});
+
+/**
+ * Launch a test 
+ */
+router.put("/launch", (req, res, next) => {
+  console.log(req.body)
+  res.json({ message: `Launch test id: ${req.body.id}` });
+});
+
+/**
+ * Update a suite 
+ */
+router.put("/update", (req, res, next) => {
+  console.log(req.body)
+  res.json({ message: `Update suite id: ${req.body.id}` });
+});
+
+/**
+ * Delete a suite
+ */
+router.delete("/delete/:id", (req, res, next) => {
+  res.json({ message: `Delete suite id: ${req.params.id}` });
 });
 
 module.exports = router;
