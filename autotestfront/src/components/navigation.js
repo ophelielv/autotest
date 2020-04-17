e4a122a273249890d9d59eea1197d2a55ac00938
Ophélie Le Vigouroux <levigouroux.oph@gmail.com>
2020-04-17 10:25:58 +0200

    fonts in progress

--- autotestfront/src/components/navigation.js
+++ autotestfront/src/components/navigation.js
@@ -14,11 +14,11 @@ function Navigation(props) {
   return (
     <nav className="App-nav">
       <ul className="level-1">
-        <li className="link-1" key="link-home"><Link to="/">Home</Link></li>
+        <li className="link-1 link-hover" key="link-home"><Link to="/">Home</Link></li>
         <li className="link-1" key="suites-title">Suites
           <ul className="level-2">
             {suites.map(suite =>
-              <li>
+              <li className="link-hover">
                 <Link className="link-2" to={`/suite/${suite.id}`} key={`link-suite-${suite.id}`}>{suite.name}</Link>
               </li>
             )}
