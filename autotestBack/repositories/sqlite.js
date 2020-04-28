a25ae564a219f67ec4721d2be4b1c948e02a9877
unknown <levigouroux.oph@gmail.com>
2020-04-28 20:47:24 +0200

    create 6 tables

--- autotestBack/repositories/sqlite.js
+++ autotestBack/repositories/sqlite.js
@@ -9,49 +9,48 @@ const db = new sqlite3.Database('./db/autotest.db', sqlite3.OPEN_READWRITE, (err
 
 db.serialize(() => {
 
-  db.run(`
-    CREATE TABLE IF NOT EXISTS Suite (
-      suite_id INTEGER PRIMARY KEY,
+  const createStmt = [
+    `CREATE TABLE IF NOT EXISTS Suite (
+      suite_id INTEGER PRIMARY KEY AUTOINCREMENT,
       code TEXT NOT NULL UNIQUE,
       name TEXT NOT NULL,
       description TEXT NOT NULL
-    );
-    CREATE TABLE IF NOT EXISTS Iteration (
-      iteration_id INTEGER PRIMARY KEY,
-      suite_id FOREIGN KEY (suite_id) 
-        REFERENCES Suite (suite_id) ON DELETE NO ACTION, ON UPDATE NO ACTION,
+    );`,
+    `CREATE TABLE IF NOT EXISTS Iteration (
+      iteration_id INTEGER PRIMARY KEY AUTOINCREMENT,
+      suite_id INTEGER FOREIGN KEY (suite_id) REFERENCES Suite (suite_id),
       date DATE,
       done INTEGER NOT NULL,
       passed INTEGER NOT NULL,
       result TEXT
-    );
-    CREATE TABLE IF NOT EXISTS Test (
-      test_id INTEGER PRIMARY KEY,
-      suite_id FOREIGN KEY (suite_id) 
-        REFERENCES Suite (suite_id) ON DELETE NO ACTION, ON UPDATE NO ACTION,
+    );`,
+    `CREATE TABLE IF NOT EXISTS Test (
+      test_id INTEGER PRIMARY KEY AUTOINCREMENT,
+      suite_id INTEGER FOREIGN KEY (suite_id) REFERENCES Suite (suite_id),
       code TEXT NOT NULL UNIQUE,
       name TEXT NOT NULL,
-      description TEXT 
-    );
-    CREATE TABLE CaseData (
-      case_data_id INTEGER PRIMARY KEY,
-      test_id FOREIGN KEY (test_id) 
-        REFERENCES Test (test_id) ON DELETE NO ACTION, ON UPDATE NO ACTION,
-      iteration_id FOREIGN KEY (iteration_id) 
-        REFERENCES Iteration (iteration_id) ON DELETE NO ACTION, ON UPDATE NO ACTION,
-    );
-    CREATE TABLE IF NOT EXISTS DataType (
-      data_type_id INTEGER PRIMARY KEY,
-      type TEXT NOT NULL UNIQUE,
-    );
-    CREATE TABLE IF NOT EXISTS CustomData (
-      custom_data_id INTEGER PRIMARY KEY,
-      case_data_id FOREIGN KEY (case_data_id) 
-        REFERENCES CaseData (case_data_id) ON DELETE NO ACTION, ON UPDATE NO ACTION,
+      description TEXT
+    );`,
+    `CREATE TABLE CaseData (
+      case_data_id INTEGER PRIMARY KEY AUTOINCREMENT,
+      test_id INTEGER FOREIGN KEY (test_id) EFERENCES Test (test_id),
+      iteration_id FOREIGN KEY (iteration_id) REFERENCES Iteration (iteration_id)
+    );`,
+    `CREATE TABLE IF NOT EXISTS DataType (
+      data_type_id INTEGER PRIMARY KEY AUTOINCREMENT,
+      type TEXT NOT NULL UNIQUE
+    );`,
+    `CREATE TABLE IF NOT EXISTS CustomData (
+      custom_data_id INTEGER PRIMARY KEY AUTOINCREMENT,
+      case_data_id INTEGER FOREIGN KEY (case_data_id) REFERENCES CaseData (case_data_id),
       name TEXT NOT NULL,
       value TEXT NOT NULL
-    );
-  `);
+    );`
+  ];
+    
+  createStmt.forEach(stmt => db.run(stmt))
+
+
 
   // const stmt = db.prepare(`INSERT INTO lorem VALUES (?)`);
 
