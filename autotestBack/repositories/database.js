78835496a86b96ee5331f94eb8daedcc49f54585
unknown <levigouroux.oph@gmail.com>
2020-05-03 20:22:02 +0200

    get suites, get suite by id, database.js

---
+++ autotestBack/repositories/database.js
@@ -0,0 +1,19 @@
+
+const sqlite3 = require('sqlite3').verbose();
+const { DATABASE_PATH } = require('../constants/db.constants');
+
+const database = new sqlite3.Database(DATABASE_PATH, sqlite3.OPEN_READWRITE, (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the in-memory SQlite database.');
+});
+
+// database.close((err) => {
+//   if (err) {
+//     return console.error(err.message);
+//   }
+//   console.log('Close the database connection.');
+// });
+
+module.exports = database;
--- autotestBack/routes/index.js
+++ autotestBack/routes/index.js
@@ -1,13 +1,14 @@
 const express = require('../node_modules/express');
 const router = express.Router();
 const MochaService = require('../services/mocha.service');
-
-// const database = require('../repositories/sqlite');
-// database.serialize();
+const database = require('../repositories/database');
 
 const bodyParser = require('body-parser');
 router.use(bodyParser.json()); // support json encoded bodies
 router.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+// const md5 = require('md5');
+// db.run(insert, ["admin", "admin@example.com", md5("admin123456")]);
 
 /**
  * GET home page.
@@ -29,17 +30,44 @@ router.get('/users', async (req, res, next) => {
 });
 
 /**
- * Get the list of all tests
+ * Get the list of all suites
  */
-router.get("/get-all/", (req, res, next) => {
-  res.json({ message: `get test id: ${req.params.id}` });
+router.get("/suites/get-all", (req, res, next) => {
+  const sql = "SELECT * FROM Suite";
+  const params = []
+  
+  database.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(400).json({"error":err.message});
+      return;
+    }
+    console.log(rows);
+    res.json({
+      "message":"success",
+      "data":rows
+    })
+  });
 });
 
 /**
  * Get info about a test suite + previous results
  */
-router.get("/get/:id", (req, res, next) => {
-  res.json({ message: `Get test id: ${req.params.id}` });
+router.get("/suite/get/:id", (req, res, next) => {
+  const sql = "SELECT * FROM Suite WHERE suite_id=?";
+  const params = [req.params.id];
+  
+  database.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(400).json({"error":err.message});
+      return;
+    }
+    console.log(rows);
+    res.json({
+        "message":"success",
+        "data":rows
+    })
+  });
+  // res.json({ message: `Get test id: ${req.params.id}` });
 });
 
 /**
