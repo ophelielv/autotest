f885287ba196e0b4b6d3371d5eaab1a716b48378
unknown <levigouroux.oph@gmail.com>
2020-07-25 00:41:28 +0200

    fix layout : background on full height

--- autotestfront/src/App.js
+++ autotestfront/src/App.js
@@ -1,37 +1,36 @@
 import React from 'react';
 import Navigation from './components/navigation';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import Suite from './scenes/suite';
 import Home from './scenes/home';
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <strong>Autotest</strong>
-        </header>
+	return (
+		<Router>
+			<div className="App">
+				<header className="App-header">
+					<strong>Autotest</strong>
+				</header>
 
-        <Navigation />
+				<Navigation />
 
-        {/* A <Switch> looks through its children <Route>s and
+				{/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/suite/:suiteId"><Suite /></Route>
-          <Route path="/"><Home /></Route>
-        </Switch>
+				<Switch>
+					<Route path="/suite/:suiteId">
+						<Suite />
+					</Route>
+					<Route path="/">
+						<Home />
+					</Route>
+				</Switch>
 
-        {/* <footer className="App-footer"> @2020 </footer> */}
-      </div>
-
-    </Router >
-  );
+				<footer className="App-footer"> @2020 </footer>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
